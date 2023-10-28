498f4386d4fee5506b750b8d7aa0e7bfe413994f
tersalman <mansalmanter@gmail.com>
2023-10-28 21:46:19 +0300

    homework is done

--- src/Checker.java
+++ src/Checker.java
@@ -1,8 +1,6 @@
 import Expection.WrongLoginException;
 import Expection.WrongPasswordException;
 
-import java.util.regex.Pattern;
-
 public class Checker {
     final static private String LOGIN_ALLOW =
             "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890_";
@@ -13,32 +11,36 @@ public class Checker {
             String password,
             String confirmPassword
     ) {
-        Checker.logginCheck(login);
-        Checker.passwordCheck(password);
-        if (!confirmPassword.equals(password)) {
-            throw new WrongPasswordException("the password is not similar");
-        }
+        Checker.loginCheck(login);
+        Checker.passwordCheck(password, confirmPassword);
 
 
     }
 
-    private static void logginCheck(String login) {
-        if (!LOGIN_ALLOW.contains(login)) {
-            throw new WrongLoginException("the logins symbols is not allowed");
+    private static void loginCheck(String login) throws WrongLoginException {
+        for (int i = 0; i < login.length(); ++i) {
+            if (LOGIN_ALLOW.indexOf(login.charAt(i)) == -1) {
+                throw new WrongLoginException("the logins symbols is not allowed");
+            }
         }
-        if (login.length()>PASS_LENGTH_ALLOW) {
+        if (login.length() > PASS_LENGTH_ALLOW) {
             throw new WrongLoginException("the logins length is not allowed");
         }
     }
-    private static void passwordCheck(String password) {
-        if (!LOGIN_ALLOW.contains(password)) {
-            throw new WrongPasswordException("the password symbols is not allowed");
+
+    private static void passwordCheck(String password, String confirmPassword) {
+        for (int i = 0; i < password.length(); ++i) {
+            if (LOGIN_ALLOW.indexOf(password.charAt(i)) == -1) {
+                throw new WrongLoginException("the password symbols is not allowed");
+            }
         }
-        if (password.length()>PASS_LENGTH_ALLOW) {
+        if (password.length() > PASS_LENGTH_ALLOW) {
             throw new WrongPasswordException("the password length is not allowed");
         }
+        if (!confirmPassword.equals(password)) {
+            throw new WrongPasswordException("the password is not similar");
+        }
     }
-
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String login = "login123";
+        String password = "wer12";
+        String confirmPassword = "wer12";
+
+        Checker.checker(login,password,confirmPassword);
+        System.out.println("hi");
     }
 }
